ecb0751513c7b15c025bee5d0b995a146e70028c
gregorionunez <colorc98@gmail.com>
2023-09-13 20:26:15 -0300

    Correccion de MateriaData
    -Metodo materiaExiste para corroborar la existencia de la materia
    -Modificacion a eliminado logico
    -Correccion de modificarMateria

--- UniversidadGrupo67/src/AccesoADatos/MateriaData.java
+++ UniversidadGrupo67/src/AccesoADatos/MateriaData.java
@@ -47,15 +47,15 @@ public class MateriaData {
     }
 
     // Metodo para modificar materia
-    public void modificarMateria(Materia materia, Materia materia2) {
+    public void modificarMateria(Materia materia) {
         // Consulta sql para insertar materia
         String sql = "UPDATE materia SET nombre=?, anio=?, estado=? WHERE idMateria =?";
         try {
             con = Conexion.getConexion(); //Conexion con la base de datos
             PreparedStatement ps = con.prepareStatement(sql); //PreparedStatement con la consulta sql
-            ps.setString(1, materia2.getNombre()); //Asignacion de valores
-            ps.setInt(2, materia2.getAnioMateria()); //Asignacion de valores
-            ps.setBoolean(3, materia2.isActivo()); //Asignacion de valores
+            ps.setString(1, materia.getNombre()); //Asignacion de valores
+            ps.setInt(2, materia.getAnioMateria()); //Asignacion de valores
+            ps.setBoolean(3, materia.isActivo()); //Asignacion de valores
             ps.setInt(4, materia.getIdMateria()); //Asignacion de valores
             ps.executeUpdate(); // Ejecutar PreparedStatement
             JOptionPane.showMessageDialog(null, "Materia modificada");
@@ -95,36 +95,41 @@ public class MateriaData {
 
     // Metodo para eliminar materia
     public void eliminarMateria(int id) {
+        if(materiaExiste(id)){
         // Consulta sql para insertar materia
-        String sql = "DELETE FROM materia WHERE idMateria=?";
+        String sql = "UPDATE materia SET estado=? WHERE idMateria=?";
         try {
             con = Conexion.getConexion(); //Conexion con la base de datos
             PreparedStatement ps = con.prepareStatement(sql); //PreparedStatement con la consulta sql
-            ps.setInt(1, id); //Asignacion de valores
+            ps.setBoolean(1, false); //Asignacion de valores
+            ps.setInt(2, id); //Asignacion de valores
             ps.executeUpdate(); // Ejecutar PreparedStatement
             JOptionPane.showMessageDialog(null, "Materia eliminada");
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia" + ex.getMessage());
         } finally {
-            Conexion.cerrarConexion(con);
+            Conexion.cerrarConexion(con); //Cerrar conexion
+        }
+        }else{
+            JOptionPane.showMessageDialog(null, "Materia inexistente");
         }
     }
-    
+
     // Metodo para listar materias
-    public List listarMaterias(){
-        
+    public List listarMaterias() {
+
         ArrayList<Materia> lista = new ArrayList<Materia>();
-        
+
         // Consulta sql para insertar materia
         String sql = "SELECT * FROM Materia";
         try {
             con = Conexion.getConexion(); //Conexion con la base de datos
             PreparedStatement ps = con.prepareStatement(sql); //PreparedStatement con la consulta sql
             ResultSet rs = ps.executeQuery(); // Ejecutar PreparedStatement
-            
-            while(rs.next()){ // Bucle para agregar elementos a la lista
-               lista.add(new Materia(rs.getInt("idMateria"), rs.getString("nombre"), rs.getInt("anio"), rs.getBoolean("estado")));
+
+            while (rs.next()) { // Bucle para agregar elementos a la lista
+                lista.add(new Materia(rs.getInt("idMateria"), rs.getString("nombre"), rs.getInt("anio"), rs.getBoolean("estado")));
             }
             JOptionPane.showMessageDialog(null, "Listado de materias completo"); // Mensaje
         } catch (SQLException ex) {
@@ -133,6 +138,31 @@ public class MateriaData {
             Conexion.cerrarConexion(con); // Cerrar conexion
         }
         return lista;
+    }
+
+    // Metodo para corroborar si existe la materia
+    public boolean materiaExiste(int id) {
+        // Consulta sql para insertar materia
+        String sql = "SELECT * FROM materia WHERE idMateria=?";
+
+        try {
+            con = Conexion.getConexion(); //Conexion con la base de datos
+            PreparedStatement ps = con.prepareStatement(sql); //PreparedStatement con la consulta sql
+            ps.setInt(1, id); //Asignacion de valores
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0; // Devuelve true si hay al menos una fila con el id
+            } else {
+                return false;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia" + ex.getMessage());
+            return false;
+        } finally {
+            Conexion.cerrarConexion(con); //Cerrar conexion
+        }
+
     }
 
 }
