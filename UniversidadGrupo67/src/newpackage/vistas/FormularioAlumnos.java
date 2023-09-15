8b7c3064043e143ec82da7fb36594d9e690ec159
LidonniGit <139288437+LidonniGit@users.noreply.github.com>
2023-09-15 19:01:03 -0300

    Modifico Ventanas Alumnos y Materias

--- UniversidadGrupo67/src/newpackage/vistas/FormularioAlumnos.java
+++ UniversidadGrupo67/src/newpackage/vistas/FormularioAlumnos.java
@@ -7,14 +7,14 @@ package newpackage.vistas;
 
 /**
  *
- * @author Usuario
+ * @author Li Donni
  */
-public class FormularioAlumno extends javax.swing.JFrame {
+public class FormularioAlumnos extends javax.swing.JInternalFrame {
 
     /**
-     * Creates new form FormularioAlumno
+     * Creates new form FormularioAlumnos
      */
-    public FormularioAlumno() {
+    public FormularioAlumnos() {
         initComponents();
     }
 
@@ -27,6 +27,13 @@ public class FormularioAlumno extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jTfNombre = new javax.swing.JTextField();
+        jRbEstado = new javax.swing.JRadioButton();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jBNuevo = new javax.swing.JButton();
+        jBSalir = new javax.swing.JButton();
+        jBEliminar = new javax.swing.JButton();
+        jBGuardar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jLabel2 = new javax.swing.JLabel();
@@ -36,28 +43,6 @@ public class FormularioAlumno extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jTfDocumento = new javax.swing.JTextField();
         jTfApellido = new javax.swing.JTextField();
-        jTfNombre = new javax.swing.JTextField();
-        jRbEstado = new javax.swing.JRadioButton();
-        jToggleButton1 = new javax.swing.JToggleButton();
-        jBNuevo = new javax.swing.JButton();
-        jBSalir = new javax.swing.JButton();
-        jBEliminar = new javax.swing.JButton();
-        jBGuardar = new javax.swing.JButton();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jLabel1.setText("Alumno");
-
-        jLabel2.setText("Documento :");
-
-        jLabel3.setText("Apellido :");
-
-        jLabel4.setText("Nombre :");
-
-        jLabel5.setText("Estado :");
-
-        jLabel6.setText("Fecha de Nacimiento :");
 
         jToggleButton1.setText("Buscar");
         jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -73,6 +58,19 @@ public class FormularioAlumno extends javax.swing.JFrame {
         jBEliminar.setText("Eliminar");
 
         jBGuardar.setText("Guardar");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setText("Alumno");
+
+        jLabel2.setText("Documento :");
+
+        jLabel3.setText("Apellido :");
+
+        jLabel4.setText("Nombre :");
+
+        jLabel5.setText("Estado :");
+
+        jLabel6.setText("Fecha de Nacimiento :");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -113,7 +111,7 @@ public class FormularioAlumno extends javax.swing.JFrame {
                                 .addComponent(jBGuardar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jBSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(125, Short.MAX_VALUE))
+                .addContainerGap(63, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -147,7 +145,7 @@ public class FormularioAlumno extends javax.swing.JFrame {
                     .addComponent(jBEliminar)
                     .addComponent(jBSalir)
                     .addComponent(jBGuardar))
-                .addContainerGap(109, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -155,43 +153,9 @@ public class FormularioAlumno extends javax.swing.JFrame {
 
     private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
         // TODO add your handling code here:
-        
+
     }//GEN-LAST:event_jToggleButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormularioAlumno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormularioAlumno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormularioAlumno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormularioAlumno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FormularioAlumno().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBEliminar;
