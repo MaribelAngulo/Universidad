0c38eaf283fd6d70b5384f6ca42236e97ed9d806
Maribel Otamendi <maribel.otamendi@medtronic.com>
2023-09-26 19:53:50 -0300

    Modificaciones en estructura de tablas y carga de datos de prueba en la base

--- UniversidadGrupo67/src/BaseDeDatos/universidadulp.sql
+++ UniversidadGrupo67/src/BaseDeDatos/universidadulp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-09-2023 a las 00:40:11
+-- Tiempo de generación: 27-09-2023 a las 00:50:24
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -42,10 +42,15 @@ CREATE TABLE `alumno` (
 
 INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES
 (4, 25253614, 'LiDonni', 'Marcelo', '2023-07-05', 1),
-(6, 26253614, 'LiDonni', 'Marcelo', '2023-07-05', 0),
-(7, 28, 'LiDonni', 'Marcelo', '2023-07-05', 1),
-(9, 30, 'LiDonni', 'Marcelo', '2023-07-05', 1),
-(10, 21, 'Maris', 'Stella', '2023-10-21', 1);
+(6, 85953614, 'Leonardo.', 'Da Vinci trs', '2023-07-05', 1),
+(7, 27955315, 'Otamendi', 'Maribel', '2023-07-05', 1),
+(9, 30578754, 'Pato', 'Lucas', '2023-07-05', 1),
+(10, 21842421, 'Maris', 'Stella', '2023-10-21', 1),
+(11, 12345, 'prueba', 'test', '2023-09-20', 1),
+(12, 65745, 'Garcia', 'Alejandro', '2023-09-05', 1),
+(13, 1, 'Torres', 'Juana', '2023-09-03', 1),
+(14, 987766, 'Pedro', 'Perez', '1987-06-17', 0),
+(15, 9869875, 'Domenico', 'Yayo', '1957-07-14', 0);
 
 -- --------------------------------------------------------
 
@@ -55,7 +60,7 @@ INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`
 
 CREATE TABLE `inscripcion` (
   `idInscripto` int(11) NOT NULL,
-  `nota` int(11) NOT NULL,
+  `nota` double NOT NULL,
   `idAlumno` int(11) NOT NULL,
   `idMateria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -66,8 +71,20 @@ CREATE TABLE `inscripcion` (
 
 INSERT INTO `inscripcion` (`idInscripto`, `nota`, `idAlumno`, `idMateria`) VALUES
 (6, 14, 10, 2),
-(7, 9, 4, 1),
-(8, 6, 4, 3);
+(10, 25, 9, 1),
+(13, 10, 9, 3),
+(14, -1, 10, 4),
+(25, 10, 7, 1),
+(28, -1, 11, 4),
+(29, -1, 11, 1),
+(30, -1, 11, 3),
+(31, -1, 6, 4),
+(33, -1, 7, 8),
+(34, -1, 7, 10),
+(35, -1, 7, 9),
+(36, -1, 7, 11),
+(37, 10, 7, 3),
+(38, -1, 11, 7);
 
 -- --------------------------------------------------------
 
@@ -87,9 +104,17 @@ CREATE TABLE `materia` (
 --
 
 INSERT INTO `materia` (`idMateria`, `nombre`, `anio`, `estado`) VALUES
-(1, 'Matematica', 2023, 1),
+(1, 'Matematica ', 2020, 1),
 (2, 'Computacion 2', 0, 0),
-(3, 'Computacion', 2023, 1);
+(3, 'Computacion', 2023, 1),
+(4, 'Salud', 2023, 1),
+(5, 'etghe', 1999, 0),
+(6, 'test', 1987, 0),
+(7, 'Deportes', 2023, 1),
+(8, 'Quimica', 2022, 1),
+(9, 'Fisica', 2021, 1),
+(10, 'Calculo', 2023, 1),
+(11, 'Algebra', 2024, 1);
 
 --
 -- Índices para tablas volcadas
@@ -114,9 +139,7 @@ ALTER TABLE `inscripcion`
 -- Indices de la tabla `materia`
 --
 ALTER TABLE `materia`
-  ADD PRIMARY KEY (`idMateria`),
-  ADD UNIQUE KEY `nombre` (`nombre`),
-  ADD UNIQUE KEY `nombre_2` (`nombre`);
+  ADD PRIMARY KEY (`idMateria`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -126,19 +149,19 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `inscripcion`
 --
 ALTER TABLE `inscripcion`
-  MODIFY `idInscripto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idInscripto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `idMateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idMateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Restricciones para tablas volcadas
